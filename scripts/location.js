ab0537fb58a52afd5436f4a3fd0f9c46677bd4de
JackTop325 <jackie.jiang22@gmail.com>
2023-03-27 13:59:17 -0400

    updated location.js to now display weather information from api

--- scripts/location.js
+++ scripts/location.js
@@ -1,5 +1,5 @@
-var longitude;
 var latitude;
+var longitude;
 
 $(document).ready(function() {
     getLocation();
@@ -13,9 +13,10 @@ $(document).ready(function() {
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-            console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+            latitude = position.coords.latitude;
+            longitude = position.coords.longitude;
+            // console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+            getWeatherInfo();
         });
     } else {
       console.log("Geolocation is not supported by this browser.");
@@ -34,12 +35,26 @@ function geoCoding() {
         success: function(data) {
           latitude = data.features[0].center[1];
           longitude = data.features[0].center[0];
-          console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+          // console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+          getWeatherInfo();
         },
         error: function(error) {
           console.log("Geocoding error: " + error);
         }
     });
 }
-  
+
+function getWeatherInfo(){
+    $.ajax({
+        url: "https://api.open-meteo.com/v1/forecast?latitude="+latitude+"&longitude="+longitude+"&hourly=temperature_2m,precipitation_probability,precipitation,windspeed_10m&timezone=America%2FNew_York",
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+          console.log(data);
+        },
+        error: function(error) {
+          console.log("Could not get weather information, Error:" + error);
+        }
+    }); 
+}
 
