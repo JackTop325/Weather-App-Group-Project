30db5d0bccb9b3560b5e9a2e71d4c1923a8b7b37
JackTop325 <jackie.jiang22@gmail.com>
2023-04-17 12:06:16 -0400

    Revert "updated Access-Control-Allow"
    This reverts commit 45bccdb2a6fdc18df970711288dcca6f61123ee7.

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const {
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
